b6d7ec181999983389565466418ce531003660e3
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2026-07-20 13:07:16 +0200

    make it easier to modify

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,67 +1,77 @@
--- preFX v0.1 - boilerplate pre-insert-fx - @sonoCircuit
+-- preFX v0.1 - pre-insert-fx boilerplate - @sonoCircuit
 
 local mod = require 'core/mods'
 
---------------------------- variables --------------------------
--- table of params without "prefx_" prefix. important: 'state' comes last!
+
+--------------------------- variables ---------------------------
+
+-- replace with your own strings
+local m = {}
+m.name = "preFX" -- UI
+m.addr = "/prefx/" -- osc address
+m.param = function(id) return "prefx_"..id end -- param prefix
+
+-- table of all param id's. important: 'state' comes last!
+-- this is required to bang the params post init.
 local paramslist = {"routing", "drive", "state"}
 
 
 --------------------------- osc msgs ---------------------------
 
-local function init_prefx()
-  osc.send({ "localhost", 57120 }, "/prefx/init")
+local function initalize()
+  osc.send({ "localhost", 57120 }, m.addr.."init")
 end
 
-local function free_prefx()
-  osc.send({ "localhost", 57120 }, "/prefx/free")
+local function free()
+  osc.send({ "localhost", 57120 }, m.addr.."free")
 end
 
 local function set_state(state)
-  osc.send({ "localhost", 57120 }, "/prefx/set_state", {state - 1})
+  osc.send({ "localhost", 57120 }, m.addr.."set_state", {state - 1})
 end
 
 local function set_bus(bus)
-  osc.send({ "localhost", 57120 }, "/prefx/set_bus", {bus - 1})
+  osc.send({ "localhost", 57120 }, m.addr.."set_bus", {bus - 1})
 end
 
--- key (string) corresponds to synthDef arg > set in params:set_action()
 local function set_param(key, val)
-  osc.send({ "localhost", 57120 }, "/prefx/set_param", {key, val})
+  osc.send({ "localhost", 57120 }, m.addr.."set_param", {key, val})
 end
+
 
 --------------------------- params ---------------------------
 local function bang_params()
   for _, prm in ipairs(paramslist) do
-    params:lookup_param("prefx_"..prm):bang()
+    params:lookup_param(m.param(prm)):bang()
   end
 end
 
-local function add_prefx()
-  params:add_group("prefx_group", "preFX", 4)
+local function add_params()
+  params:add_group(m.param("group"), m.name, 4)
 
-  params:add_option("prefx_state", "state", {"off", "on"}, 1)
-  params:set_action("prefx_state", function(state) set_state(state) end)
+  params:add_option(m.param("state"), "state", {"off", "on"}, 1)
+  params:set_action(m.param("state"), function(state) set_state(state) end)
 
-  params:add_option("prefx_routing", "routing", {"pre-eng", "post-eng"}, 1)
-  params:set_action("prefx_routing", function(bus) set_bus(bus) end)
+  params:add_option(m.param("routing"), "routing", {"pre-eng", "post-eng"}, 1)
+  params:set_action(m.param("routing"), function(bus) set_bus(bus) end)
 
-  -----------------------------------------------------------------------------
-  params:add_separator("prefx_settings", "settings")
+---------------------------/ user zone /---------------------------
+  params:add_separator(m.param("settings"), "settings")
 
   -- add all params required by your synthdef. 
-  params:add_control("prefx_drive", "drive", controlspec.new(0, 1, "lin", 0, 0), function(param) return util.round(param:get() * 100, 1).."%" end)
-  params:set_action("prefx_drive", function(val) set_param('drive', val) end)
+  params:add_control(m.param("drive"), "drive", controlspec.new(0, 1, "lin", 0, 0), function(param) return util.round(param:get() * 100, 1).."%" end)
+  params:set_action(m.param("drive"), function(val) set_param('drive', val) end) -- use set_param('myArg', val) to set the sc params
 
-  -----------------------------------------------------------------------------
-  -- manually bang params.
-  -- do not use params:bang() as we don't want to bang all script params post init.
+---------------------------------/---------------------------------
+
+  -- manually bang params to avoid using params:bang() as we don't want to bang all script params post init.
   bang_params()
 
 end
 
+
 --------------------------- mod zone ---------------------------
 
-mod.hook.register("system_post_startup", "preFX post startup", init_prefx)
-mod.hook.register("script_post_init", "preFX post init", add_prefx)
-mod.hook.register("script_post_cleanup", "preFX cleanup", free_prefx)
+mod.hook.register("system_post_startup", m.name.." post startup", initalize)
+mod.hook.register("script_post_init", m.name.." post init", add_params)
+mod.hook.register("script_post_cleanup", m.name.." cleanup", free)
